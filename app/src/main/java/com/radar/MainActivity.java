ccf7c822b9b60231a539136cbda2071f6ff863be
ZhouQi <1296129243@qq.com>
2017-08-24 15:22:07 +0800

    Initial commit

---
+++ app/src/main/java/com/radar/MainActivity.java
@@ -0,0 +1,18 @@
+package com.radar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        RadarView mRadarView = (RadarView) findViewById(R.id.radar);
+//        mRadarView.setMainPaintColor(Color.BLUE);
+//        mRadarView.setTextPaintColor(Color.GRAY);
+//        mRadarView.setValuePaintColor(Color.RED);
+    }
+}
